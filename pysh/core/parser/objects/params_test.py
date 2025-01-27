885c4bb29294110fab296ff6b50f4630c0cd0531
Dave Carr <davecarr1024@gmail.com>
2025-01-27 14:44:17 -0500

    params_test

--- pysh/core/parser/objects/params_test.py
+++ pysh/core/parser/objects/params_test.py
@@ -1,6 +1,6 @@
 from pysh.core import regex
-from pysh.core.parser import Parser
-from pysh.core.parser.objects import Param, Params
+from pysh.core.parser import Parser, State
+from pysh.core.parser.objects import Arg, Param, Params
 
 _int = Parser.head("int", regex.Regex.digits().one_or_more()).value().transform(int)
 
@@ -21,3 +21,13 @@ def test_combine(subtests):
     ):
         with subtests.test(params=params):
             assert params == Params.for_children(a, b, c, d), params
+
+
+def test_apply():
+    assert (_int.param("a") & _int.param("b")).ignore_whitespace()("123 456") == (
+        State(),
+        [
+            Arg[int]("a", 123),
+            Arg[int]("b", 456),
+        ],
+    )
